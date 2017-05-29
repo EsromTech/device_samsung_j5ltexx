d8c27a35a7706d07aa3d089f4d8e9a8caa1beb50
EsromG5 <esromgalaxy5@gmail.com>
2017-05-29 21:57:16 +0000

    This is PureNexus

--- aosp_j5ltexx.mk
+++ aosp_j5ltexx.mk
@@ -1,6 +1,3 @@
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
 # Inherit device j5ltexx
 $(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
 
@@ -10,6 +7,3 @@ PRODUCT_NAME := aosp_j5ltexx
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RELEASE_NAME := j5ltexx
 TARGET_DEVICE := j5lte
-
-# Export official support
-EXTENDED_BUILD_TYPE=OFFICIAL
